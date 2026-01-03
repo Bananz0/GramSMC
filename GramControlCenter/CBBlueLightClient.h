267eeb0635a2458eeed7a3e80bdb6ef60653654a
Glen Muthoka <glenmuthokaq@gmail.com>
2026-01-03 12:12:14 +0000

    feat: enhance CBBlueLightClient header with additional CCT control methods and status updates

--- GramControlCenter/CBBlueLightClient.h
+++ GramControlCenter/CBBlueLightClient.h
@@ -5,7 +5,8 @@
 //  Based on CBBlueLightClient from Shifty by Nate Thompson
 //  https://github.com/thompsonate/Shifty
 //
-//  Partial header for CBBlueLightClient in private CoreBrightness API
+//  Enhanced header for CBBlueLightClient in private CoreBrightness API
+//  Includes additional methods for CCT control and status
 //
 
 #import <Foundation/Foundation.h>
@@ -32,6 +33,13 @@ typedef struct {
     BOOL available;
 } Status;
 
+typedef struct {
+    float minCCT;
+    float maxCCT;
+    float midCCT;
+} CCTRange;
+
+// Existing methods
 - (BOOL)setStrength:(float)strength commit:(BOOL)commit;
 - (BOOL)setEnabled:(BOOL)enabled;
 - (BOOL)setMode:(int)mode;
@@ -40,5 +48,23 @@ typedef struct {
 - (BOOL)getBlueLightStatus:(Status *)status;
 - (void)setStatusNotificationBlock:(void (^)(void))block;
 + (BOOL)supportsBlueLightReduction;
+
+// Enhanced methods for CCT (Color Temperature) control
+- (BOOL)setCCT:(float)cct commit:(BOOL)commit;
+- (BOOL)setCCT:(float)cct withPeriod:(float)period commit:(BOOL)commit;
+- (BOOL)getCCT:(float *)cct;
+- (BOOL)getCCTRange:(CCTRange *)range;
+- (BOOL)setCCTRange:(CCTRange *)range;
+- (BOOL)getDefaultCCTRange:(CCTRange *)range;
+
+// Additional status and control methods
+- (BOOL)setActive:(BOOL)active;
+- (BOOL)setEnabled:(BOOL)enabled withOption:(int)option;
+- (BOOL)getWarningCCT:(float *)cct;
+- (BOOL)getWarningStrength:(float *)strength;
+- (void)suspendNotifications:(BOOL)suspend;
+- (void)suspendNotifications:(BOOL)suspend force:(BOOL)force;
+- (void)enableNotifications;
+- (void)disableNotifications;
 
 @end
